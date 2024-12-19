7815bf49e9cee23ac55e8b9f87440efd917ebaf1
Marek Eibel <536376@mail.muni.cz>
2024-12-19 22:12:09 +0100

    feat: create cpp file VrmlNodeManager

--- vrmlproc/src/parser/services/VRMLNodeManager.hpp
+++ vrmlproc/src/parser/services/VRMLNodeManager.hpp
@@ -3,44 +3,22 @@
 #include <string>
 #include <unordered_map>
 #include <memory>
-#include <iostream>
 
 #include "VrmlNode.hpp"
+#include "VrmlProcessingExport.hpp"
 
 namespace vrml_proc {
     namespace parser {
 
-        class VrmlNodeManager {
+        class VRMLPROCESSING_API VrmlNodeManager {
         public:
             VrmlNodeManager() = default;
 
-            void AddDefinitionNode(const std::string& id, std::shared_ptr<VrmlNode> node) {
-                m_definitionNodes[id] = node;
-            }
+            void AddDefinitionNode(const std::string& id, std::shared_ptr<VrmlNode> node);
 
-            std::shared_ptr<VrmlNode> GetDefinitionNode(const std::string& id) const {
-                auto it = m_definitionNodes.find(id);
-                if (it != m_definitionNodes.end()) {
-                    return it->second;
-                }
-                return nullptr;
-            }
+            std::shared_ptr<VrmlNode> GetDefinitionNode(const std::string& id) const;
 
-            inline size_t GetDefNodesTotalCount() const {
-                return m_definitionNodes.size();
-            }
-
-            void Display(std::ostream &output_stream) const {
-                if (GetDefNodesTotalCount() == 0) {
-                    output_stream << "There are no DEF nodes stored." << std::endl;
-                    return;
-                }
-                for (auto& [id, node_reference] : m_definitionNodes) {
-                    if (node_reference != nullptr) {
-                        output_stream << *node_reference << std::endl;
-                    }
-                }
-            }
+            inline size_t GetDefNodesTotalCount() const { return m_definitionNodes.size(); }
 
         private:
             std::unordered_map<std::string, std::shared_ptr<VrmlNode>> m_definitionNodes;
