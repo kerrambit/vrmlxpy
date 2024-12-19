b18d5fc6ca099ee2807c77bbc5d729069088d1dd
Marek Eibel <536376@mail.muni.cz>
2024-12-19 12:17:05 +0100

    feat: remove most of fucntions from vrml_processing files
    Note that these files are tmp and will be removed completely

--- vrmlproc/src/parser/vrml_processing.hpp
+++ vrmlproc/src/parser/vrml_processing.hpp
@@ -3,25 +3,10 @@
 #include <string>
 #include <optional>
 
-#include "ParserResult.hpp"
-#include "VRMLFile.hpp"
-
 #include "VrmlProcessingExport.hpp"
 
 namespace vrml_proc {
     namespace parser {
-
-        VRMLPROCESSING_API void printMessage();
-
-        VRMLPROCESSING_API bool parseVec3f(std::string& text);
-
-        VRMLPROCESSING_API bool parseVec4f(std::string& text);
-
-        VRMLPROCESSING_API bool parseVec3fArray(std::string& text);
-
-        VRMLPROCESSING_API bool parseInt32Array(std::string& text);
-
-        VRMLPROCESSING_API ParserResult<VRMLFile> ParseVrmlFile(std::string& text);
 
         using LoadFileResult = std::optional<std::string>;
 
