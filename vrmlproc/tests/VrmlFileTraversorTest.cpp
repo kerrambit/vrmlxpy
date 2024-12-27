70e8f0d46313e59b25aceea2c7fb3a1679315ef5
Marek Eibel <536376@mail.muni.cz>
2024-12-27 17:56:24 +0100

    feat: add test for vrmlfile traversor, does not work at the moment

---
+++ vrmlproc/tests/VrmlFileTraversorTest.cpp
@@ -0,0 +1,64 @@
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+#include <vector>
+#include <memory>
+
+#include <boost/variant/get.hpp>
+#include <boost/variant/recursive_wrapper.hpp>
+#include <boost/variant/static_visitor.hpp>
+#include <boost/variant/variant.hpp>
+
+#include "test-data/VrmlFileTraversorTestDataset.hpp"
+#include <boost/optional/optional.hpp>
+#include <boost/variant/detail/apply_visitor_unary.hpp>
+#include <Int32Array.hpp>
+#include <ParserResult.hpp>
+#include <UseNode.hpp>
+#include <Vec3f.hpp>
+#include <Vec3fArray.hpp>
+#include <VrmlField.hpp>
+#include <VrmlFile.hpp>
+#include <VrmlNode.hpp>
+#include <VrmlNodeManager.hpp>
+#include <VrmlParser.hpp>
+#include <VrmlUnits.hpp>
+#include <SimpleFileReader.hpp>
+#include <MemoryMappedFileReader.hpp>
+#include <VrmlFileTraversor.hpp>
+#include <MeshConversionContext.hpp>
+
+static vrml_proc::parser::ParserResult<vrml_proc::parser::VrmlFile> ParseVrmlFile(std::string& text, vrml_proc::parser::VrmlNodeManager& manager) {
+
+    vrml_proc::parser::VrmlParser parser(manager);
+    return parser.Parse(text);
+}
+
+static vrml_proc::parser::ParserResult<vrml_proc::parser::VrmlFile> ParseVrmlFile(const std::filesystem::path& filepath, vrml_proc::parser::VrmlNodeManager& manager) {
+
+    vrml_proc::core::MemoryMappedFileReader reader;
+    auto readResult = reader.LoadFile(filepath);
+    if (!readResult.has_value()) {
+        return {};
+    }
+
+    vrml_proc::parser::VrmlParser parser(manager);
+    return parser.Parse(readResult.value());
+}
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------ //
+
+//TEST_CASE("Parse VRML File - Valid Input - Simple VRML File", "[parsing][valid]") {
+//
+//    vrml_proc::parser::VrmlNodeManager manager;
+//    auto parseResult = ParseVrmlFile(emptyChildren, manager);
+//    REQUIRE(parseResult);
+//
+//    std::cout << parseResult.value().at(1);
+//
+//    vrml_proc::traversor::VrmlFileTraversor traversor;
+//    auto traversorResult = traversor.Traverse({ parseResult.value(), manager});
+//
+//    std::shared_ptr<vrml_proc::conversion_context::MeshConversionContext> meshContextPtr = std::dynamic_pointer_cast<vrml_proc::conversion_context::MeshConversionContext>(traversorResult);
+//    REQUIRE(meshContextPtr);
+//}
