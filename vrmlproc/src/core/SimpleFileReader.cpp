3303a783ada3e038455d890f040fd814fc0fefbb
Marek Eibel <536376@mail.muni.cz>
2024-12-27 09:25:32 +0100

    feat: add debugging info when in Debug configuration
    note: this will be reworked in the future when logging is introduced

--- vrmlproc/src/core/SimpleFileReader.cpp
+++ vrmlproc/src/core/SimpleFileReader.cpp
@@ -5,17 +5,32 @@
 #include <fstream>
 #include <sstream>
 
+#ifdef DEBUG
+#include "ScopedTimer.hpp"
+#endif
+
 vrml_proc::core::FileReader::LoadFileResult vrml_proc::core::SimpleFileReader::LoadFile(const std::filesystem::path& filepath)
 {
-    std::ifstream file(filepath.string(), std::ios::in);
+#ifdef DEBUG
+    double time;
+#endif
+    std::string fileContents;
+    {
+#ifdef DEBUG
+        auto timer = vrmlproc::core::utils::ScopedTimer(time);
+#endif
+        std::ifstream file(filepath.string(), std::ios::in);
 
-    if (!file) {
-        return { };
+        if (!file) {
+            return { };
+        }
+
+        std::stringstream buffer;
+        buffer << file.rdbuf();
+        fileContents = buffer.str();
     }
-
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    std::string file_contents = buffer.str();
-
-    return { std::move(file_contents) };
+#ifdef DEBUG
+    std::cout << "Time to read and load file <" << filepath.string() << "> into string took " << time << " seconds." << std::endl;
+#endif
+    return { std::move(fileContents) };
 }
