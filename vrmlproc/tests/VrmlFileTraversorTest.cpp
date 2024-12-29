953d7595c62ccd29e313fc3827f01a2db52a9cba
Marek Eibel <536376@mail.muni.cz>
2024-12-29 17:02:07 +0100

    fix: fix the first test

--- vrmlproc/tests/VrmlFileTraversorTest.cpp
+++ vrmlproc/tests/VrmlFileTraversorTest.cpp
@@ -3,6 +3,7 @@
 
 #include <memory>
 #include <vector>
+#include <any>
 
 #include <boost/variant/get.hpp>
 #include <boost/variant/recursive_wrapper.hpp>
@@ -27,6 +28,8 @@
 #include <VrmlNodeManager.hpp>
 #include <VrmlParser.hpp>
 #include <VrmlUnits.hpp>
+#include <BaseConversionContextActionMap.hpp>
+#include <SpotlightAction.hpp>
 
 static vrml_proc::parser::ParserResult<vrml_proc::parser::VrmlFile> ParseVrmlFile(std::string& text, vrml_proc::parser::VrmlNodeManager& manager) {
 
@@ -48,15 +51,26 @@ static vrml_proc::parser::ParserResult<vrml_proc::parser::VrmlFile> ParseVrmlFil
 
 // ------------------------------------------------------------------------------------------------------------------------------------------------ //
 
-//TEST_CASE("Parse VRML File - Valid Input - Simple VRML File", "[parsing][valid]") {
-//
-//    vrml_proc::parser::VrmlNodeManager manager;
-//    auto parseResult = ParseVrmlFile(validGroup, manager);
-//    REQUIRE(parseResult);
-//
-//    vrml_proc::traversor::VrmlFileTraversor traversor;
-//    auto traversorResult = traversor.Traverse({ parseResult.value(), manager});
-//
-//    std::shared_ptr<vrml_proc::conversion_context::MeshConversionContext> meshContextPtr = std::dynamic_pointer_cast<vrml_proc::conversion_context::MeshConversionContext>(traversorResult);
-//    REQUIRE(meshContextPtr);
-//}
+TEST_CASE("Parse VRML File - Valid Input - Simple VRML File", "[parsing][valid]") {
+
+    vrml_proc::parser::VrmlNodeManager manager;
+    auto parseResult = ParseVrmlFile(validGroup, manager);
+    REQUIRE(parseResult);
+
+    vrml_proc::action::BaseConversionContextActionMap actionMap;
+    actionMap.AddAction("Spotlight", [](const std::vector<std::any>& args) {
+        if (args.size() == 1 && args[0].type() == typeid(float)) {
+            return std::make_shared<vrml_proc::action::SpotlightAction>(std::any_cast<float>(args[0]));
+        }
+        throw std::invalid_argument("Invalid arguments for SpotlightAction");
+        });
+
+    vrml_proc::traversor::VrmlFileTraversor traversor;
+    auto traversorResult = traversor.Traverse({ parseResult.value(), manager}, actionMap);
+
+    std::shared_ptr<vrml_proc::conversion_context::MeshConversionContext> meshContextPtr = std::dynamic_pointer_cast<vrml_proc::conversion_context::MeshConversionContext>(traversorResult);
+    REQUIRE(meshContextPtr);
+
+    auto meshContext = meshContextPtr->GetData();
+    REQUIRE(meshContext.size() == 1);
+}
