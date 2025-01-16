b7bdc964c8557b8784761fc3ffefb9ca5d9037f2
Marek Eibel <536376@mail.muni.cz>
2025-01-16 15:34:15 +0100

    feat: add utility function to get string of type

---
+++ vrml_proc/src/core/utils/TypeToString.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+
+#include <typeinfo>
+#ifdef __GNUG__
+#include <cxxabi.h>
+#include <cstdlib>
+#endif
+
+namespace vrml_proc {
+	namespace core {
+		namespace utils {
+			/**
+			 * @brief Utility function which returns a string representing the name of the type.
+			 * @note The __cxa_demangle function was found here https://gcc.gnu.org/onlinedocs/libstdc++/libstdc++-html-USERS-4.3/a01696.html.
+			 */
+			template<typename T>
+			std::string TypeToString(const T& object) {
+
+#ifdef __GNUG__
+				int status;
+				char* demangled = abi::__cxa_demangle(typeid(object).name(), nullptr, nullptr, &status);
+				if (status == 0) {
+					std::string result(demangled);
+					std::free(demangled);
+					return result;
+				}
+#endif
+				return typeid(object).name();
+			}
+		}
+	}
+}
