2a9e2468885e7c82207f9f7f3ec9928c4904c612
Marek Eibel <536376@mail.muni.cz>
2025-01-16 21:02:02 +0100

    feat: add function version of TypeToString withotu parameter

--- vrml_proc/src/core/utils/TypeToString.hpp
+++ vrml_proc/src/core/utils/TypeToString.hpp
@@ -12,7 +12,7 @@ namespace vrml_proc {
 	namespace core {
 		namespace utils {
 			/**
-			 * @brief Utility function which returns a string representing the name of the type.
+			 * @brief Utility function which returns a string representing the name of the type based on the passed const reference to an entity.
 			 * @note The __cxa_demangle function was found here https://gcc.gnu.org/onlinedocs/libstdc++/libstdc++-html-USERS-4.3/a01696.html.
 			 */
 			template<typename T>
@@ -28,6 +28,25 @@ namespace vrml_proc {
 				}
 #endif
 				return typeid(object).name();
+			}
+
+			/**
+			 * @brief Utility function which returns a string representing the name of the type based on the template typename argument.
+			 * @note The __cxa_demangle function was found here https://gcc.gnu.org/onlinedocs/libstdc++/libstdc++-html-USERS-4.3/a01696.html.
+			 */
+			template<typename T>
+			std::string TypeToString() {
+
+#ifdef __GNUG__
+				int status;
+				char* demangled = abi::__cxa_demangle(typeid(T).name(), nullptr, nullptr, &status);
+				if (status == 0) {
+					std::string result(demangled);
+					std::free(demangled);
+					return result;
+				}
+#endif
+				return typeid(T).name();
 			}
 		}
 	}
