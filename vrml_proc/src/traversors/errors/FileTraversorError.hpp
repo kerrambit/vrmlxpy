99c7c87dae26977a9cf339fec6e800207357719c
Marek Eibel <536376@mail.muni.cz>
2025-01-22 18:30:34 +0100

    feat: add traversor errors

---
+++ vrml_proc/src/traversors/errors/FileTraversorError.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <memory>
+#include <sstream>
+#include <string>
+
+#include "Error.hpp"
+#include "VrmlNode.hpp"
+
+namespace vrml_proc {
+	namespace traversor {
+		namespace error {
+			class FileTraversorError : public vrml_proc::core::error::Error {
+			public:
+				FileTraversorError(std::shared_ptr<vrml_proc::core::error::Error> innerError, const vrml_proc::parser::VrmlNode& errorNode)
+					: m_node(errorNode) {
+					SetInnerError(innerError);
+				}
+				const vrml_proc::parser::VrmlNode& GetErrorNode() const {
+					return m_node;
+				}
+			protected:
+				std::string GetMessageInternal() const override {
+					std::ostringstream stream;
+					stream << "[FileTraversorError]: when traversing root <"
+						<< m_node.header << ">, unexpected error occurred!\n";
+					return stream.str();
+				}
+			private:
+				const vrml_proc::parser::VrmlNode& m_node;
+
+				static const vrml_proc::parser::VrmlNode& GetDefaultNode() {
+					static vrml_proc::parser::VrmlNode dummyNode;
+					return dummyNode;
+				}
+			};
+		}
+	}
+}
