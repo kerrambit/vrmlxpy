34124f3a0d22b6d9dbaf22fce3f9923c98a97d0d
Marek Eibel <536376@mail.muni.cz>
2025-01-22 18:29:32 +0100

    feat: add null pointer error class

---
+++ vrml_proc/src/core/errors/NullPointerError.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <memory>
+#include <sstream>
+#include <string>
+
+namespace vrml_proc {
+	namespace core {
+		namespace error {
+			class NullPointerError : public Error {
+			protected:
+				std::string GetMessageInternal() const override {
+					return "[NullPointerError]: (smart) pointer contained unexpectedly null pointer!\n";
+				}
+			};
+		}
+	}
+}
