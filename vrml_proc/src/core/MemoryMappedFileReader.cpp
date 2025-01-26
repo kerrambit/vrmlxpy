3d39dca6f66a9c5050c989767bc417bcc67a9d40
Marek Eibel <536376@mail.muni.cz>
2025-01-26 08:11:05 +0100

    feat: add logging into reader and parser

--- vrml_proc/src/core/MemoryMappedFileReader.cpp
+++ vrml_proc/src/core/MemoryMappedFileReader.cpp
@@ -1,29 +1,24 @@
 #include "MemoryMappedFileReader.hpp"
 
-#include <boost/iostreams/device/mapped_file.hpp>
-
-#include <iostream>
 #include <string>
 
-#ifdef RELEASE
+#include <boost/iostreams/device/mapped_file.hpp>
+
+#include "Logger.hpp"
 #include "ScopedTimer.hpp"
-#endif
 
 vrml_proc::core::FileReader::LoadFileResult vrml_proc::core::MemoryMappedFileReader::LoadFile(const std::filesystem::path& filepath)
 {
-#ifdef RELEASE
     double time;
-#endif
+    std::size_t fileSize;
     std::string fileContents;
     {
-#ifdef RELEASE
         auto timer = vrmlproc::core::utils::ScopedTimer(time);
-#endif
         boost::iostreams::mapped_file mappedFile(filepath.string(), boost::iostreams::mapped_file::readonly);
-        fileContents =  mappedFile.const_data();
+        fileContents = mappedFile.const_data();
+        fileSize = mappedFile.size();
     }
-#ifdef RELEASE
-    std::cout << "Time to read and load file <" << filepath.string() << "> into string took " << time << " seconds." << std::endl;
-#endif
+
+    LOG_INFO() << "Time to read and load file <" << filepath.string() << "> of size " << fileSize << " bytes into string took " << time << " seconds.";
     return { std::move(fileContents) };
 }
--- vrml_proc/src/parser/VrmlParser.cpp
+++ vrml_proc/src/parser/VrmlParser.cpp
@@ -2,6 +2,9 @@
 
 #include <boost/spirit/include/qi.hpp>
 
+#include "Logger.hpp"
+#include "ScopedTimer.hpp"
+
 namespace vrml_proc {
 
 	namespace parser {
@@ -11,12 +14,19 @@ namespace vrml_proc {
             auto iterator = string.begin();
             std::vector<VrmlNode> parsedData;
 
-            bool success = boost::spirit::qi::phrase_parse(iterator, string.end(), m_grammar, m_skipper, parsedData);
+            double time;
+            bool success;
+            {
+                auto timer = vrmlproc::core::utils::ScopedTimer(time);
+                success = boost::spirit::qi::phrase_parse(iterator, string.end(), m_grammar, m_skipper, parsedData);
+            }
 
             if (success && iterator == string.end()) {
+                LOG_INFO() << "Parsing was successful. The whole parsing and AST creation process took " << time << " seconds.";
                 return std::optional(parsedData);
             }
 
+            LOG_ERROR() << "Parsing was not successful. Process took " << time << " seconds.";
             return {};
 		}
 	}
