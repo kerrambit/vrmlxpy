0b2a1b10f9db972b55719662c4417a65e70378b1
Marek Eibel <536376@mail.muni.cz>
2025-01-28 17:03:54 +0100

    feat: add object address into print

--- vrml_proc/src/parser/models/VrmlField.cpp
+++ vrml_proc/src/parser/models/VrmlField.cpp
@@ -9,12 +9,12 @@ struct Visitor {
     Visitor(std::ostream *outputStream, Printable::IndentationLevel indentationLevel) : outputStream(outputStream), indentationLevel(indentationLevel) {}
 
     void operator()(const std::string& string) const {
-        *outputStream << Printable::CreateIndentationString(indentationLevel) << "String: <" << string << ">";
+        *outputStream << Printable::CreateIndentationString(indentationLevel) << "String (" << &string << "): <" << string << ">";
         *outputStream << std::endl;
     }
 
     void operator()(const bool& boolean) const {
-        *outputStream << Printable::CreateIndentationString(indentationLevel) << "Boolean: <" << std::boolalpha << boolean << ">";
+        *outputStream << Printable::CreateIndentationString(indentationLevel) << "Boolean (" << &boolean << "): <" << std::boolalpha << boolean << ">";
         *outputStream << std::endl;
     }
 
@@ -27,12 +27,12 @@ struct Visitor {
     }
 
     void operator()(vrml_proc::parser::float32_t value) const {
-        *outputStream << Printable::CreateIndentationString(indentationLevel) << "Float32: <" << value << ">";
+        *outputStream << Printable::CreateIndentationString(indentationLevel) << "Float32 (" << &value << "): <" << value << ">";
         *outputStream << std::endl;
     }
 
     void operator()(int32_t value) const {
-        *outputStream << Printable::CreateIndentationString(indentationLevel) << "Int32: <" << value << ">";
+        *outputStream << Printable::CreateIndentationString(indentationLevel) << "Int32 (" << &value << "): <" << value << ">";
         *outputStream << std::endl;
     }
 
@@ -53,7 +53,7 @@ struct Visitor {
     }
 
     void operator()(const std::vector<boost::variant<boost::recursive_wrapper<vrml_proc::parser::VrmlNode>, boost::recursive_wrapper<vrml_proc::parser::UseNode>>>& nodes) const {
-        *outputStream << Printable::CreateIndentationString(indentationLevel) << "VRMLNodeArray:\n";
+        *outputStream << Printable::CreateIndentationString(indentationLevel) << "VRMLNodeArray (" << &nodes << "):\n";
         *outputStream << Printable::CreateIndentationString(indentationLevel + 1) << "[\n";
         
         struct PrintVisitor {
@@ -82,7 +82,7 @@ struct Visitor {
 void vrml_proc::parser::VrmlField::Print(Printable::IndentationLevel indentationLevel) const {
     std::string indentation_string = Printable::CreateIndentationString(indentationLevel);
     *Printable::AccessStreamPointer() << indentation_string;
-    *Printable::AccessStreamPointer() << "VRMLField:\n";
+    *Printable::AccessStreamPointer() << "VRMLField (" << this << "):\n";
     *Printable::AccessStreamPointer() << Printable::CreateIndentationString(indentationLevel + 1) << "Name: <" << name << ">\n";
     *Printable::AccessStreamPointer() << Printable::CreateIndentationString(indentationLevel + 1) << "Value:\n";
     boost::apply_visitor(Visitor(Printable::AccessStreamPointer(), indentationLevel + 2), value);
