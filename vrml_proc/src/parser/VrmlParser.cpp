74cb433332427106c7e89b8d52e6cf8cc677d9df
Marek Eibel <536376@mail.muni.cz>
2025-01-28 17:12:59 +0100

    fix: vrml parser now populates manager after parsing

--- vrml_proc/src/parser/VrmlParser.cpp
+++ vrml_proc/src/parser/VrmlParser.cpp
@@ -1,9 +1,17 @@
 #include "VrmlParser.hpp"
 
-#include <boost/spirit/include/qi.hpp>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include <boost/spirit/home/qi/parse.hpp>
 
 #include "Logger.hpp"
+#include "ParserResult.hpp"
 #include "ScopedTimer.hpp"
+#include "VrmlFile.hpp"
+#include "VrmlNode.hpp"
+#include "VrmlNodeManagerPopulator.hpp"
 
 namespace vrml_proc {
 
@@ -11,11 +19,13 @@ namespace vrml_proc {
 
         ParserResult<VrmlFile> VrmlParser::Parse(std::string& string)
 		{
+            LOG_INFO() << "Parse VRML file content.";
+
             auto iterator = string.begin();
             std::vector<VrmlNode> parsedData;
 
-            double time;
-            bool success;
+            double time = 0.0;
+            bool success = false;
             {
                 auto timer = vrmlproc::core::utils::ScopedTimer(time);
                 success = boost::spirit::qi::phrase_parse(iterator, string.end(), m_grammar, m_skipper, parsedData);
@@ -23,7 +33,18 @@ namespace vrml_proc {
 
             if (success && iterator == string.end()) {
                 LOG_INFO() << "Parsing was successful. The whole parsing and AST creation process took " << time << " seconds.";
-                return std::optional(parsedData);
+
+                LOG_INFO() << "Populate VrmlNodeManager with DEF nodes.";
+                double time = 0.0;
+                {
+                    auto timer = vrmlproc::core::utils::ScopedTimer(time);
+                    for (const auto& root : parsedData) {
+                        VrmlNodeManagerPopulator::Populate(m_manager, root);
+                    }
+                }
+                LOG_INFO() << "DEF nodes populating has finished. The whole process took " << time << " seconds.";
+
+                return parsedData;
             }
 
             LOG_ERROR() << "Parsing was not successful. Process took " << time << " seconds.";
