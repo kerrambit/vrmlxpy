8ec9e11f6d93b9f69b5702cacd300ab21529d78c
Marek Eibel <536376@mail.muni.cz>
2025-02-04 20:48:13 +0100

    feat: devide and create new test for calculators (WIP)

--- to_stl/tests/VrmlFileTraversorTest.cpp
+++ to_stl/tests/VrmlFileTraversorTest.cpp
@@ -8,6 +8,7 @@
 
 #include "test_data/VrmlFileTraversorTestDataset.hpp"
 #include <BoxAction.hpp>
+#include <BoxCalculator.hpp>
 #include <ConversionContextActionMap.hpp>
 #include <GroupAction.hpp>
 #include <Logger.hpp>
@@ -20,8 +21,6 @@
 #include <VrmlFileTraversor.hpp>
 #include <VrmlNodeManager.hpp>
 #include <VrmlParser.hpp>
-#include <test.hpp>
-#include <BoxCalculator.hpp>
 
 static vrml_proc::parser::ParserResult<vrml_proc::parser::VrmlFile> ParseVrmlFile(std::string& text, vrml_proc::parser::VrmlNodeManager& manager) {
 
@@ -45,6 +44,10 @@ static void HandleRootLevelError(const cpp::result<std::shared_ptr<vrml_proc::co
     if (result.has_error()) {
         vrml_proc::core::logger::LogUnformattedText("caught application error", result.error()->GetMessage(), vrml_proc::core::logger::Level::Error, LOGGING_INFO);
     }
+}
+
+static void HandleRootLevelError(std::shared_ptr<vrml_proc::core::error::Error> error) {
+    vrml_proc::core::logger::LogUnformattedText("caught application error", error->GetMessage(), vrml_proc::core::logger::Level::Error, LOGGING_INFO);
 }
 
 static vrml_proc::action::ConversionContextActionMap<vrml_proc::conversion_context::MeshConversionContext>& GetActionMap() {
@@ -102,22 +105,6 @@ static vrml_proc::action::ConversionContextActionMap<vrml_proc::conversion_conte
 TEST_CASE("Initialization") {
 
     vrml_proc::core::logger::InitLogging();
-
-    vrml_proc::parser::Vec3f size;
-    size.x = 2.0; size.y = 2.0; size.z = 2.0;
-
-    to_stl::calculator::BoxCalculator calculator = to_stl::calculator::BoxCalculator();
-    auto result = (calculator.Generate3DMesh({ std::cref(size) })).value();
-
-    size.x = 10.0; size.y = 20.0; size.z = 30.0;
-    auto result2 = (calculator.Generate3DMesh({ std::cref(size) })).value();
-
-    export_to_stl(*result, R"(C:\Users\marek\Documents\FI_MUNI\sem_05\SBAPR\vrmlxpy\export.stl)");
-    export_to_stl(*result2, R"(C:\Users\marek\Documents\FI_MUNI\sem_05\SBAPR\vrmlxpy\export.stl)");
-    to_stl::core::Mesh mesh;
-    mesh.join(*result);
-    mesh.join(*result2);
-    export_to_stl(mesh, R"(C:\Users\marek\Documents\FI_MUNI\sem_05\SBAPR\vrmlxpy\export.stl)");
 }
 
 TEST_CASE("Parse VRML File - Valid Input - Simple VRML File - Box node", "[parsing][valid]") {
