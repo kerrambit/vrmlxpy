e7020f70904a1a17768bc1207606f8d1bc82e79f
Marek Eibel <536376@mail.muni.cz>
2025-02-04 20:42:37 +0100

    feat: add Range class into core::utils

---
+++ vrml_proc/src/core/utils/Range.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Comparable.hpp"
+
+namespace vrml_proc {
+	namespace core {
+		namespace utils {
+
+			template <contract::Comparable T>
+			struct Range {
+
+				Range(T min, T max)
+					: min(min), max(max) {}
+
+				Range()
+					: min(-std::numeric_limits<T>::infinity()), max(std::numeric_limits<T>::infinity()) {}
+
+				void SetMinimum(T min) {
+					this->min = min;
+				}
+
+				void SetMaximum(T max) {
+					this->max = max;
+				}
+
+				bool CheckValueInRangeExclusive(T value) {
+					return !(value <= min || value >= max);
+				}
+
+				bool CheckValueInRangeInclusive(T value) {
+					return !(value < min || value > max);
+				}
+
+				friend std::ostream& operator<<(std::ostream& os, const Range& range) {
+					return os << "Range: { min: <" << range.min << ">, max: <" << range.max << "> }";
+				}
+
+				T max;
+				T min;
+			};
+		}
+	}
+}
