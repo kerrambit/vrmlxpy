a1f4f29d09809ef3ff4bed95d698cd639a2d69f4
Marek Eibel <536376@mail.muni.cz>
2025-02-04 20:37:35 +0100

    feat: add << operator to join (add) inner errors

--- vrml_proc/src/core/errors/Error.hpp
+++ vrml_proc/src/core/errors/Error.hpp
@@ -31,6 +31,23 @@ namespace vrml_proc {
 				const std::shared_ptr<Error> GetInnerError() const {
 					return m_innerError;
 				}
+				Error& operator<<(std::shared_ptr<Error> innerError) {
+					SetInnerError(innerError);
+					return *this;
+				}
+				static std::shared_ptr<Error> GetInnermostError(std::shared_ptr<Error> error) {
+
+					if (error == nullptr) {
+						return nullptr;
+					}
+
+					std::shared_ptr<Error> curr = error;
+					while (curr->GetInnerError() != nullptr) { 
+						curr = curr->GetInnerError();
+					}
+
+					return curr;
+				}
 			protected:
 				virtual std::string GetMessageInternal() const = 0;
 				size_t m_indentation = 0;
@@ -40,4 +57,11 @@ namespace vrml_proc {
 
 		}
 	}
+}
+
+inline std::shared_ptr<vrml_proc::core::error::Error> operator<<(std::shared_ptr<vrml_proc::core::error::Error> left, std::shared_ptr<vrml_proc::core::error::Error> right) {
+	if (left) {
+		*left << right;
+	}
+	return left;
 }
