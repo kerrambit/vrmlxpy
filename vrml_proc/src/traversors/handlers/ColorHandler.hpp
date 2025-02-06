f7c721fb094fd8c3e91e74ba84ce3982f66c3ce6
Marek Eibel <536376@mail.muni.cz>
2025-02-06 19:55:24 +0100

    feat: implement other handlers (WIP)

---
+++ vrml_proc/src/traversors/handlers/ColorHandler.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+
+#include <any>
+#include <result.hpp>
+
+#include "ConversionContextActionExecutor.hpp"
+#include "ConversionContextActionMap.hpp"
+#include "ColorNodeValidator.hpp"
+#include "Error.hpp"
+#include "Logger.hpp"
+#include "MeshConversionContext.hpp"
+#include "NodeTraversorError.hpp"
+#include "Vec3fArray.hpp"
+#include "FullParsedVrmlNodeContext.hpp"
+
+#include "VrmlProcessingExport.hpp"
+
+namespace vrml_proc::traversor::handler::ColorHandler {
+    template<typename ConversionContext>
+    VRMLPROCESSING_API cpp::result<std::shared_ptr<ConversionContext>, std::shared_ptr<vrml_proc::core::error::Error>>
+        Handle(
+            vrml_proc::traversor::FullParsedVrmlNodeContext context,
+            const vrml_proc::action::ConversionContextActionMap<ConversionContext>& actionMap
+        );
+}
---
+++ vrml_proc/src/traversors/handlers/NormalHandler.cpp
@@ -0,0 +1,48 @@
+#include "NormalHandler.hpp"
+
+#include <memory>
+
+#include <any>
+#include <result.hpp>
+
+#include "ConversionContextActionExecutor.hpp"
+#include "ConversionContextActionMap.hpp"
+#include "Error.hpp"
+#include "Logger.hpp"
+#include "MeshConversionContext.hpp"
+#include "NodeTraversorError.hpp"
+#include "NormalNodeValidator.hpp"
+#include "Vec3fArray.hpp"
+
+#include "VrmlProcessingExport.hpp"
+
+template<typename ConversionContext>
+cpp::result<std::shared_ptr<ConversionContext>, std::shared_ptr<vrml_proc::core::error::Error>> vrml_proc::traversor::handler::NormalHandler::Handle(vrml_proc::traversor::FullParsedVrmlNodeContext context, const vrml_proc::action::ConversionContextActionMap<ConversionContext>& actionMap)
+{
+    LOG_INFO() << "Handle VRML node <" << context.node.header << ">.";
+
+    vrml_proc::traversor::validator::NormalNodeValidator validator(context.node);
+    auto validationResult = validator.Validate();
+    if (validationResult.has_error()) {
+        std::shared_ptr<vrml_proc::core::error::Error> error = std::make_shared<vrml_proc::traversor::error::NodeTraversorError>(validationResult.error(), context.node);
+        return cpp::fail(error);
+    }
+    static vrml_proc::parser::Vec3fArray defaultVector;
+    std::any cachedVector = validator.GetCachedVector(defaultVector);
+
+    return vrml_proc::traversor::utils::ConversionContextActionExecutor::TryToExecute<ConversionContext>(actionMap, context.node.header, { std::cref(cachedVector) }, { });
+}
+
+namespace vrml_proc {
+    namespace traversor {
+        namespace handler {
+            namespace NormalHandler {
+
+                template VRMLPROCESSING_API cpp::result<std::shared_ptr<vrml_proc::conversion_context::MeshConversionContext>, std::shared_ptr<vrml_proc::core::error::Error>>
+                    Handle<vrml_proc::conversion_context::MeshConversionContext>(
+                        vrml_proc::traversor::FullParsedVrmlNodeContext,
+                        const vrml_proc::action::ConversionContextActionMap<vrml_proc::conversion_context::MeshConversionContext>&);
+            }
+        }
+    }
+}
