965ac98a0b8043cea32184c3b6aa1cb45ea09667
Marek Eibel <536376@mail.muni.cz>
2025-02-07 17:05:32 +0100

    feat: wip indexed face set action

--- to_stl/src/actions/IndexedFaceSetAction.cpp
+++ to_stl/src/actions/IndexedFaceSetAction.cpp
@@ -14,22 +14,20 @@
 #include "VrmlFieldExtractor.hpp"
 #include "VrmlNodeManager.hpp"
 #include "VrmlNodeTraversor.hpp"
+#include <StlBaseStructure.hpp>
 
-//class HelperCoordAction : public vrml_proc::action::ConversionContextAction<to_stl::conversion_context::CoordConversionContext> {
-//public:
-//	HelperCoordAction(const vrml_proc::traversor::handler::IndexedFaceSetHandler::IndexedFaceSetProperties& properties)
-//		: m_properties(properties) {}
-//
-//	std::shared_ptr<to_stl::conversion_context::CoordConversionContext> Execute() {
-//
-//		// auto result = vrml_proc::parser::model::utils::VrmlFieldExtractor::ExtractByName<vrml_proc::parser::Vec3fArray>("point", m_properties.coord.get().fields);
-//		// std::reference_wrapper<const vrml_proc::parser::Vec3fArray> data = result.value();
-//		return std::make_shared<to_stl::conversion_context::CoordConversionContext>(5);
-//	}
-//
-//private:
-//	const vrml_proc::traversor::handler::IndexedFaceSetHandler::IndexedFaceSetProperties& m_properties;
-//};
+class HelperCoordAction : public vrml_proc::action::ConversionContextAction<to_stl::conversion_context::CoordConversionContext> {
+public:
+	HelperCoordAction(std::reference_wrapper<const vrml_proc::parser::Vec3fArray> data)
+		: m_data(data) {}
+
+	std::shared_ptr<to_stl::conversion_context::CoordConversionContext> Execute() {
+		return std::make_shared<to_stl::conversion_context::CoordConversionContext>(m_data);
+	}
+
+private:
+	std::reference_wrapper<const vrml_proc::parser::Vec3fArray> m_data;
+};
 
 namespace to_stl {
 	namespace action {
@@ -48,21 +46,39 @@ namespace to_stl {
 			}
 
 			vrml_proc::parser::VrmlNodeManager manager;
-
 			vrml_proc::action::ConversionContextActionMap<conversion_context::CoordConversionContext> map;
-			/*map.AddAction("Coordinate", [this](const auto& refArgs, const auto& copyArgs) {
+
+			map.AddAction("Coordinate", [this](const auto& refArgs, const auto& copyArgs) {
 				if (refArgs.size() == 1 && refArgs[0].get().type() == typeid(std::reference_wrapper<const vrml_proc::parser::Vec3fArray>)) {
 					auto coord = std::any_cast<std::reference_wrapper<const vrml_proc::parser::Vec3fArray>>(refArgs[0]);
-					return std::make_shared<HelperCoordAction>(m_properties);
+					return std::make_shared<HelperCoordAction>(coord);
 				}
-
 				assert(false && "Invalid arguments for CoordinateAction");
-				});*/
+			});
+
+			map.AddAction("Normal", [this](const auto& refArgs, const auto& copyArgs) {
+				if (refArgs.size() == 1 && refArgs[0].get().type() == typeid(std::reference_wrapper<const vrml_proc::parser::Vec3fArray>)) {
+					auto coord = std::any_cast<std::reference_wrapper<const vrml_proc::parser::Vec3fArray>>(refArgs[0]);
+					return std::make_shared<HelperCoordAction>(coord);
+				}
+				assert(false && "Invalid arguments for NormalAction");
+				});
 
 			const vrml_proc::parser::VrmlNode& node = m_properties.coord.get();
 			vrml_proc::traversor::FullParsedVrmlNodeContext context = vrml_proc::traversor::FullParsedVrmlNodeContext(node, manager, false);
 
-			// vrml_proc::traversor::VrmlNodeTraversor::Traverse<conversion_context::CoordConversionContext>(context, map);
+			auto coordResult = vrml_proc::traversor::VrmlNodeTraversor::Traverse<conversion_context::CoordConversionContext>(context, map);
+			if (coordResult.has_value()) {
+				std::shared_ptr<to_stl::conversion_context::CoordConversionContext> coord = coordResult.value();
+			}
+
+			auto normalResult = vrml_proc::traversor::VrmlNodeTraversor::Traverse<conversion_context::CoordConversionContext>(context, map);
+			if (normalResult.has_value()) {
+				std::shared_ptr<to_stl::conversion_context::CoordConversionContext> normal = normalResult.value();
+			}
+
+			vrml_proc::conversion_context::StlBaseStructure entity = vrml_proc::conversion_context::StlBaseStructure({ 1.0, 1.0, 1.0 }, { {1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}, {1.0, 1.0, 1.0} });
+			result->Add(std::move(entity));
 
 			return result;
 		}
--- to_stl/src/conversion_context/CoordConversionContext.hpp
+++ to_stl/src/conversion_context/CoordConversionContext.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <functional>
-#include <optional>
+#include <vector>
 
 #include "BaseConversionContext.hpp"
 #include "Vec3fArray.hpp"
@@ -10,30 +10,31 @@ namespace to_stl::conversion_context {
 
 	class CoordConversionContext : public vrml_proc::conversion_context::BaseConversionContext {
 	public:
-		CoordConversionContext(std::reference_wrapper<const vrml_proc::parser::Vec3fArray> data)
-			: m_data(data) {}
+		CoordConversionContext(std::reference_wrapper<const vrml_proc::parser::Vec3fArray> data) {
+			m_data.push_back(data);
+		}
 
 		CoordConversionContext() = default;
 
 		void Merge(Mergeable* other) override {
-			/*if (other == nullptr) {
+			if (other == nullptr) {
 				return;
 			}
 			CoordConversionContext* otherContext = dynamic_cast<CoordConversionContext*>(other);
 			if (otherContext) {
-				if (otherContext->m_data.get().vectors.size() == 0) {
+				if (otherContext->m_data.size() == 0) {
 					return;
 				}
-				m_data.get().vectors.reserve(m_data.get().vectors.size() + otherContext->m_data.get().vectors.size());
-				m_data.get().insert(m_data.get().vectors.end(), std::make_move_iterator(otherContext->m_data.get().vectors.begin()), std::make_move_iterator(otherContext->m_data.get().vectors.end()));
-				otherContext->m_data.get().vectors.clear();
+				m_data.reserve(m_data.size() + otherContext->m_data.size());
+				m_data.insert(m_data.end(), std::make_move_iterator(otherContext->m_data.begin()), std::make_move_iterator(otherContext->m_data.end()));
+				otherContext->m_data.clear();
 			}
 			else {
 				throw std::invalid_argument("Merge called with incompatible type!");
-			}*/
+			}
 		}
 
 	private:
-		std::optional<std::reference_wrapper<const vrml_proc::parser::Vec3fArray>> m_data = std::nullopt;
+		std::vector<std::reference_wrapper<const vrml_proc::parser::Vec3fArray>> m_data = {};
 	};
 }
