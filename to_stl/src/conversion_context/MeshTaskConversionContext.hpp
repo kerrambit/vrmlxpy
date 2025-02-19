f9c9d6a5850d4d7f2736e0fdfd820c4d1b189ca9
Marek Eibel <536376@mail.muni.cz>
2025-02-19 22:14:26 +0100

    feat: create new object MeshTask and new conversion context MeshTaskConversionContext
    MeshTask is a task which will create an mesh

---
+++ to_stl/src/conversion_context/MeshTaskConversionContext.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "MeshTask.hpp"
+#include "VectorConversionContext.hpp"
+
+namespace to_stl::conversion_context {
+	using MeshTaskConversionContext = vrml_proc::conversion_context::VectorConversionContext<to_stl::core::MeshTask>;
+}
