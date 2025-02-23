984d5e0fae930ab3afc76d69a537d4be2aca1ec4
Marek Eibel <536376@mail.muni.cz>
2025-02-23 17:33:36 +0100

    feat: parser result is now cpp::result

--- vrml_proc/src/parser/ParserResult.hpp
+++ vrml_proc/src/parser/ParserResult.hpp
@@ -1,10 +1,14 @@
 #pragma once
 
-#include <optional>
+#include <memory>
+
+#include <result.hpp>
+
+#include "Error.hpp"
 
 namespace vrml_proc {
 	namespace parser {
 		template <typename T>
-		using ParserResult = std::optional<T>;
+		using ParserResult = cpp::result<T, std::shared_ptr<vrml_proc::core::error::Error>>;
 	}
 }
