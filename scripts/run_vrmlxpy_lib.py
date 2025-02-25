e517e74fd4866b15e8ac85de84fe95a115974e84
Marek Eibel <536376@mail.muni.cz>
2025-02-25 17:04:41 +0100

    fix: reanme parameter

--- scripts/run_vrmlxpy_lib.py
+++ scripts/run_vrmlxpy_lib.py
@@ -2,10 +2,11 @@ from vrmlxpy import *
 import sys
 
 if len(sys.argv) != 3:
-    print(f"Usage: {sys.argv[0]} <input_path> <output_directory>")
+    print(f"Usage: {sys.argv[0]} <input_file> <output_file>")
     sys.exit(1)
 
 input_filename = sys.argv[1]
-output_directory = sys.argv[2]
+output_filename = sys.argv[2]
 
-convert_vrml_to_stl(input_filename, output_directory)
+print_version()
+convert_vrml_to_stl(input_filename, output_filename)
