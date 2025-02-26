34bec9213dfe5c1b4fdb293292c68356be4c3b18
Marek Eibel <536376@mail.muni.cz>
2025-02-26 20:49:40 +0100

    feat: add method gathering all non-canonical header names for node

--- vrml_proc/src/traversors/node_descriptors/VrmlCanonicalHeaders.hpp
+++ vrml_proc/src/traversors/node_descriptors/VrmlCanonicalHeaders.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
+#include <unordered_set>
 #include <string>
 #include <map>
 
 namespace vrml_proc::traversor::node_descriptor {
 
+    /**
+     * @brief Groups all posible header names and thei mapping to canonical VRML header name.
+     * Each pair is {POSSIBLE_NAME, CANONICAL_FORM}.
+     */
     std::map<std::string, std::string> HeaderToCanonicalName = {
 
         {"Group", "Group"},
@@ -19,7 +24,39 @@ namespace vrml_proc::traversor::node_descriptor {
         {"IndexedFaceSet", "IndexedFaceSet"},
         {"VRMLIndexedFaceSet", "IndexedFaceSet"},
 
+        {"Box", "Box"},
+        {"VRMLBox", "Box"},
+
         {"Shape", "Shape"},
         {"VRMLShape", "Shape"},
+
+        {"Color", "Color"},
+        {"VRMLColor", "Color"},
+
+        {"Coordinate", "Coordinate"},
+        {"VRMLCoordinate", "Coordinate"},
+
+        {"Normal", "Normal"},
+        {"VRMLNormal", "Normal"},
+
+        {"TextureCoordinate", "TextureCoordinate"},
+        {"VRMLTextureCoordinate", "TextureCoordinate"},
+
+        {"Appearance", "Appearance"},
+        {"VRMLAppearance", "Appearance"},
+
+        {"WorldInfo", "WorldInfo"},
+        {"VRMLWorldInfo", "WorldInfo"},
     };
+
+    inline std::unordered_set<std::string> GetPossibleNamesForCanonical(const std::string& canonicalName) {
+
+        std::unordered_set<std::string> result;
+        for (const auto& [possible, canonical] : HeaderToCanonicalName) {
+            if (canonical == canonicalName) {
+                result.insert(possible);
+            }
+        }
+        return result;
+    }
 }
