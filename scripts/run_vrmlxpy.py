92645d835c3f03a404ae4e64afacf9717c3447c5
Marek Eibel <536376@mail.muni.cz>
2025-03-07 16:44:20 +0100

    fix: missing module name

--- scripts/run_vrmlxpy.py
+++ scripts/run_vrmlxpy.py
@@ -9,5 +9,5 @@ input_filename = sys.argv[1]
 output_filename = sys.argv[2]
 config_filename = sys.argv[3]
 
-print_version()
+vrmlxpy.vrmlxpy.print_version()
 vrmlxpy.vrmlxpy.convert_vrml(input_filename, output_filename, config_filename)
