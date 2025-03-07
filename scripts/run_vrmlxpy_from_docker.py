6a86f2d1927f1f6ba99078eacbc41ba846fbc86f
Marek Eibel <536376@mail.muni.cz>
2025-03-07 14:59:34 +0100

    fix: have 2 python scripts, one for pypi and other for locally run

---
+++ scripts/run_vrmlxpy_from_docker.py
@@ -0,0 +1,13 @@
+from vrmlxpy import *
+import sys
+
+if len(sys.argv) != 4:
+    print(f"Usage: {sys.argv[0]} <input_file> <output_file> <config_file>")
+    sys.exit(1)
+
+input_filename = sys.argv[1]
+output_filename = sys.argv[2]
+config_filename = sys.argv[3]
+
+print_version()
+vrmlxpy.convert_vrml(input_filename, output_filename, config_filename)
