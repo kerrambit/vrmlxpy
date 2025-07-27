571eedd07b4f3b37eaf888d96e5277b71f237e3c
Marek Eibel <536376@mail.muni.cz>
2025-07-27 18:54:22 +0200

    fix: avoid writing vrmlxpy.vrmlxpy

--- pypi/vrmlxpy/__init__.py
+++ pypi/vrmlxpy/__init__.py
@@ -3,6 +3,9 @@ import sys
 
 # Load the compiled extension
 if sys.platform.startswith("win"):
-    from .vrmlxpy import *  # Windows: Imports `vrmlxpy.pyd`
+    from . import *  # Windows: Imports `vrmlxpy.pyd`
 elif sys.platform.startswith("linux"):
-    from .vrmlxpy import *  # Linux: Imports `vrmlxpy.so`
+    from . import *  # Linux: Imports `vrmlxpy.so`
+
+import vrmlxpy.vrmlxpy as _impl
+sys.modules[__name__] = _impl
