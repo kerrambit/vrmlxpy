c01d56673c28c7bfb3e73527a3b6661c9706780d
Marek Eibel <536376@mail.muni.cz>
2026-02-25 17:56:11 +0100

    fix: fix the pypi setup and init files

--- pypi/setup.py
+++ pypi/setup.py
@@ -1,29 +1,8 @@
-import os
-import platform
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools.dist import Distribution
 
-if platform.system() == "Windows":
-    binary_files = ["vrmlxpy/vrmlxpy.pyd", "vrmlxpy/vrmlproc.dll", "vrmlxpy/tostl.dll"]
-elif platform.system() == "Linux":
-    binary_files = ["vrmlxpy/vrmlxpy.so", "vrmlxpy/libvrmlproc.so", "vrmlxpy/libtogeom.so", "vrmlxpy/libboost_iostreams.so.1.74.0", "vrmlxpy/libboost_log.so.1.74.0", "vrmlxpy/libboost_thread.so.1.74.0", "vrmlxpy/libboost_filesystem.so.1.74.0"]
-else:
-    binary_files = []
+class BinaryDistribution(Distribution):
+    def has_ext_modules(self):
+        return True
 
-setup(
-    name="vrmlxpy",
-    version="1.0.4",
-    author="Marek Eibel",
-    description="Toolkit for VRML parsing and traversing. Includes a standalone VRML parser library and a conversion library for transforming VRML geometry into geometry format such as STL, with modular C++ backends and Python bindings.",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/kerrambit/vrmlxpy",
-    include_package_data=True,
-    classifiers=[
-         "Programming Language :: Python :: 3",
-         "Programming Language :: C++",
-         "Operating System :: Microsoft :: Windows",
-         "Operating System :: POSIX :: Linux",
-    ],
-    python_requires=">=3.10",
-    zip_safe=False,
-)
+setup(distclass=BinaryDistribution)
--- pypi/vrmlxpy/__init__.py
+++ pypi/vrmlxpy/__init__.py
@@ -1,11 +1,1 @@
-import os
-import sys
-
-# Load the compiled extension
-if sys.platform.startswith("win"):
-    from . import *  # Windows: Imports `vrmlxpy.pyd`
-elif sys.platform.startswith("linux"):
-    from . import *  # Linux: Imports `vrmlxpy.so`
-
-import vrmlxpy.vrmlxpy as _impl
-sys.modules[__name__] = _impl
+from .vrmlxpy import *
